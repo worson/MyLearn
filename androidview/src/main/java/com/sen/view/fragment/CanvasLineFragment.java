0da59b621f5a9ddb2d79f65b97a9d6ed8cf88724
王圣星 <wang.sheng.xing@qq.com>
2016-05-24 15:37:08 +0800

    更新画路径功能

--- androidview/src/main/java/com/sen/view/fragment/CanvasLineFragment.java
+++ androidview/src/main/java/com/sen/view/fragment/CanvasLineFragment.java
@@ -1,12 +1,15 @@
 package com.sen.view.fragment;
 
 
+import android.annotation.TargetApi;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.TextPaint;
@@ -18,7 +21,9 @@ import android.widget.ImageView;
 
 import com.sen.view.R;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -60,26 +65,48 @@ public class CanvasLineFragment extends Fragment {
 
     Bitmap mStrategyRouteBitmap = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_8888);
 
+
+    private void initPath(int strategy,List<Point> pointList){
+        pointList.clear();
+        textPoints.clear();
+        loadNames.clear();
+        switch (strategy){
+            case 0:
+                mPathPoints.add(new Point(0,0));
+                mPathPoints.add(new Point(50,0));
+                mPathPoints.add(new Point(50,50));
+                mPathPoints.add(new Point(0,50));
+                mPathPoints.add(new Point(0,100));
+                mPathPoints.add(new Point(0,150));
+                mPathPoints.add(new Point(50,150));
+                mPathPoints.add(new Point(50,200));
+
+                textPoints.add(new Point(50,30));
+                textPoints.add(new Point(0,150));
+                textPoints.add(new Point(30,150));
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+        loadNames.addAll(Arrays.asList(new String[]{"深南大道立交桥","南海大道","学府路","滨海大道","无名路","107国道"}));
+
+    }
+
     private void updatePath(){
-        mPathPoints.clear();
-        mPathPoints.add(new Point(0,0));
-        mPathPoints.add(new Point(100,0));
-        mPathPoints.add(new Point(0,100));
-        mPathPoints.add(new Point(50,-50));
-        mPathPoints.add(new Point(100,100));
-        mPathPoints.add(new Point(0,0));
-        mPathPoints.add(new Point(200,0));
-        mPathPoints.add(new Point(200,200));
-
-
-
+        initPath(0,mPathPoints);
         int height = 500;
         int width = 500;
 
-        List<Point> newPointList = scalePonitPath(500,500,mPathPoints);
+        RectMapPara mRectMapPara = measureRect(500,500,mPathPoints);
+        List<Point> newPointList = rectRemap(mPathPoints,mRectMapPara);
         if (newPointList != null) {
-            Bitmap bitmap = updatePathBitmap(width,height,newPointList, mStrategyRouteBitmap);
-            drawPathText(bitmap);
+            Bitmap srcBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);;
+            Bitmap bitmap = drawPath(newPointList,srcBitmap);
+            drawPathText(rectRemap(textPoints,mRectMapPara),loadNames,3,bitmap);
             mStrategyRouteView.setImageBitmap(bitmap);
         }
 
@@ -98,10 +125,36 @@ public class CanvasLineFragment extends Fragment {
     }
 
     private List<Point> mPathPoints = new ArrayList<Point>();
+    private List<Point> textPoints = new ArrayList<Point>();
+    private List<String> loadNames = new ArrayList<>();
+
     private Paint mPathPaint = new Paint(Color.YELLOW);
 
-    private List<Point> scalePonitPath(int width, int height, List<Point> points){
+    private class RectMapPara
+    {
 
+        public Point refPoint;
+        public double scalefactor;
+        public int widthMove;
+        public int heightMove;
+    }
+
+    private List<Point> rectRemap(List<Point> points,RectMapPara rectMapPara){
+        Point refPoint = rectMapPara.refPoint;
+        double scalefactor = rectMapPara.scalefactor;
+        int widthMove = rectMapPara.widthMove;
+        int heightMove = rectMapPara.heightMove;
+        List<Point> newPointList = new LinkedList<Point>();
+        for(Point point: points){
+            int newX = (int)((point.x-refPoint.x)/scalefactor)+widthMove;
+            int newY = (int)((point.y-refPoint.y)/scalefactor)+heightMove;
+            Point newPoint = new Point(newX,newY);
+            newPointList.add(newPoint);
+        }
+        return newPointList;
+    }
+
+    private RectMapPara measureRect(int width, int height, List<Point> points){
         if (points == null || width ==0 || height ==0) {
             return null;
         }
@@ -110,7 +163,7 @@ public class CanvasLineFragment extends Fragment {
         int maxLeft,maxTop,maxRight,maxBottom;//取名风格以左下为参照
         int maxLeftIndex,maxTopIndex,maxRightIndex,maxBottomIndex;
         int pointWidth,pointHeight,widthMove,heightMove;
-        final int paddingWidth=10,paddingHeight=10;
+        final int paddingWidth=20,paddingHeight=20;
         int index = 0;
         boolean isHeightScale = true;//缩放后较小的进行居中处理，true时表示高度
         double widthScalefactor = 1, heightScalefactor =1,scalefactor =1;
@@ -164,22 +217,162 @@ public class CanvasLineFragment extends Fragment {
 
         widthMove = widthMove+paddingWidth;
         heightMove = heightMove+paddingHeight;
-        for(Point point: points){
-            int newX = (int)((point.x-refPoint.x)/scalefactor)+widthMove;
-            int newY = (int)((point.y-refPoint.y)/scalefactor)+heightMove;
-            Point newPoint = new Point(newX,newY);
-            newPointList.add(newPoint);
+
+        RectMapPara rectMapPara = new RectMapPara();
+        rectMapPara.refPoint = refPoint;
+        rectMapPara.scalefactor = scalefactor;
+        rectMapPara.heightMove = heightMove;
+        rectMapPara.widthMove = widthMove;
+
+        return  rectMapPara;
+    }
+    
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private  Bitmap drawPathText(List<Point> points, List<String> loads, int counter, Bitmap bitmap){
+        final int TEXT_MARGIN_WIDTH = 15;
+        final int TEXT_MARGIN_HEIGHT = 15;
+        int moveH =0,moveV=0,baseX=0,baseY=0;
+        Canvas canvas = new Canvas(bitmap);
+        Paint textPaint = new TextPaint();
+        List<Path> drawPathList = new LinkedList<>();
+        textPaint.setStrokeWidth(10);
+        textPaint.setTextSize(24);
+        textPaint.setColor(Color.WHITE);
+        Point centerPoint = new Point(bitmap.getWidth()/2,bitmap.getHeight()/2);
+        Paint.FontMetrics fm = textPaint.getFontMetrics();
+        int textHeight = (int) (Math.ceil(fm.descent - fm.ascent));
+        Path intersectPath = new Path();
+
+        //最长的路优先显示，优先取路的中间坐标点，多条路名左右交叉
+        final int Cnt = Math.min(counter,points.size());
+        for (int i = 0; i < Cnt; i++) {
+            Point point = points.get(i);
+            String content = loads.get(i);
+            if (content != null) {
+                if(content.length()>6){
+                    content = content.substring(0,5)+"...";
+                }
+                float tw = textPaint.measureText(content);
+                float th = Math.abs(textPaint.descent() + textPaint.ascent());
+                int pathtop = 0;
+                HaloLogger.logI(TAG,"正在绘制文本："+i+" "+content+"..."+"长度为："+content.length()+" 长："+(int)tw+" 高:"+textHeight);
+                for (int strategy = 0; strategy < 3; strategy++) {
+                    //计算当前点与路径的大概关系
+                    switch (strategy){
+                        case 0://向右下生长
+                            moveH = TEXT_MARGIN_WIDTH;
+                            moveV = textHeight+TEXT_MARGIN_HEIGHT;
+                            pathtop = point.y+TEXT_MARGIN_HEIGHT;
+                            break;
+                        case 1://向左下生长
+                            moveH = -((int)textHeight+TEXT_MARGIN_WIDTH);
+                            moveV = textHeight+TEXT_MARGIN_HEIGHT;
+                            pathtop = point.y+TEXT_MARGIN_HEIGHT;
+                            break;
+                        case 2://右上生长
+                            moveH = TEXT_MARGIN_WIDTH;
+                            moveV = -(TEXT_MARGIN_HEIGHT);
+                            pathtop = point.y-TEXT_MARGIN_HEIGHT-textHeight;
+                            break;
+                        default:
+                            break;
+                    }
+                    //右上生成
+                    baseX = point.x+moveH;
+                    baseY = point.y+moveV;
+                    //右上生长
+                    Path currentPath = getTextPath(baseX,pathtop,baseX+(int)tw,pathtop+textHeight);
+//                    Path currentPath = getTextPath(new Point(baseX,baseY),(int)tw,textHeight-moveH);
+//                    Path currentPath = getRectPath(new Point(baseX,pathtop),(int)tw,-textHeight);
+                    boolean calculateOk = false;
+                    for (int pathIndext = 0; pathIndext < drawPathList.size(); pathIndext++){
+                        Path path = drawPathList.get(pathIndext);
+                        if (intersectPath.op(currentPath,path,Path.Op.INTERSECT)){//计算成功
+                            if(intersectPath.isEmpty()) {//未相交  path.op(currentPath,Path.Op.INTERSECT)
+                                if(pathIndext == (drawPathList.size()-1)){
+                                    calculateOk = true;
+                                }
+                            }else {
+                                HaloLogger.logI(TAG,"绘制文本："+content+"与PATH相交");
+                                calculateOk = false;
+                                break;
+                            }
+
+                        }else{
+                            HaloLogger.logI(TAG,"相交计算不成功");
+                        }
+                    }
+                    if(drawPathList.size()<=0){
+                        calculateOk = true;
+                    }
+                    if (calculateOk){//测量成功
+                        HaloLogger.logI(TAG,"完成绘制文本："+i+" "+content);
+                        canvas.drawText(content,baseX,baseY,textPaint);
+                        drawPathList.add(currentPath);
+
+//                        canvas.drawTextOnPath(content,currentPath,0,0,textPaint);
+
+                        Paint paint = new Paint();
+                        paint.setStrokeWidth(2);
+                        paint.setStyle(Paint.Style.STROKE);
+                        paint.setColor(Color.RED);
+                        canvas.drawPath(currentPath,paint);
+
+                        Path line = new Path();
+                        line.moveTo(point.x,point.y);
+                        line.lineTo(point.x+tw,point.y);
+                        canvas.drawPath(line,paint);
+
+                        break;
+                    }
+                }
+
+            }
+
         }
-        HaloLogger.logI(TAG,"源数据点为："+points);
-        HaloLogger.logI(TAG,"源数据左右上下的宽度分别为："+maxLeft+", "+maxRight+", "+maxTop+", "+maxBottom);
-        HaloLogger.logI(TAG,"源数据左上参考点为："+refPoint.x+", "+refPoint.y);
-        HaloLogger.logI(TAG,"转换后点："+newPointList);
-
-
-        return newPointList;
+        return bitmap;
     }
 
-    private Bitmap updatePathBitmap(int loads, int height,List<Point> points,Bitmap bitmap){
+    //右上生成
+    private Path getTextPath(float left, float top, float right, float bottom){
+
+        Path path = new Path();
+        //float left, float top, float right, float bottom, Direction dir
+        path.addRect(left,top,right,bottom, Path.Direction.CCW);
+        return path;
+    }
+
+    //右上生成
+    private Path getTextPath(Point refPoint,int width,int height){
+
+        Path path = new Path();
+        //float left, float top, float right, float bottom, Direction dir
+        path.addRect(refPoint.x,refPoint.y,refPoint.x+width,refPoint.y-height, Path.Direction.CCW);
+        return path;
+    }
+
+    // TODO: 16/5/23 未实现
+    private Path getRectPath(Point refPoint,int width,int height){
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(refPoint.x,refPoint.y));
+        points.add(new Point(refPoint.x+width,refPoint.y));
+        points.add(new Point(refPoint.x+width,refPoint.y+height));
+        points.add(new Point(refPoint.x,refPoint.y+height));
+//        points.add(new Point(refPoint.x,refPoint.y));
+        Path path = new Path();
+
+        Point moveTo = points.get(0);
+        path.moveTo(moveTo.x,moveTo.y);
+        for (int i = 1; i <points.size() ; i=i+1) {
+            Point toPoint = points.get(i);
+            path.lineTo(toPoint.x,toPoint.y);
+        }
+        path.close();
+//        HaloLogger.logI(TAG,"getRectPath ：path is "+path);
+        return path;
+    }
+
+    private Bitmap drawPath(List<Point> points,Bitmap bitmap){
 
         mPathPaint.setColor(Color.BLUE);
         mPathPaint.setStrokeWidth(10);
@@ -193,15 +386,14 @@ public class CanvasLineFragment extends Fragment {
         Point endPoint = points.get(points.size()-1);
 
         Paint textPaint = new TextPaint();
-        textPaint.setStrokeWidth(10);
         textPaint.setTextSize(20);
         textPaint.setColor(Color.WHITE);
-//        canvas.drawText("无名路",20,20,textPaint);
 
         int colorCnt = 0;
-        final String[] LOADS = new String[]{"深南大道","南海大道","学府路","滨海大道"};
+        String[] LOADS = new String[]{"深南大道","南海大道","学府路","滨海大道","无名路","107车道"};
+        int[] COLORS  = new int[]{Color.BLUE,Color.RED,Color.BLUE,Color.BLUE,Color.RED,Color.BLUE};
 
-        int step = points.size()/5;//points.size()/20
+        int step = points.size()/1;//points.size()/20
         int index = 0;
         for (int j = 0; j <points.size()/step ; j=j+1) {
             Path path = new Path();
@@ -215,32 +407,27 @@ public class CanvasLineFragment extends Fragment {
                 Point toPoint = points.get(index);
                 path.lineTo(toPoint.x,toPoint.y);
             }
-            if(Math.random()>0.7){
-                canvas.drawTextOnPath(LOADS[(int)((LOADS.length-1)*Math.random())],path,0,20,textPaint);
-                if(++colorCnt>2){
-                    colorCnt=0;
-                    mPathPaint.setColor(Color.RED);
-
-                }
-            }
             canvas.drawPath(path,mPathPaint);
             mPathPaint.setColor(Color.BLUE);
         }
 
-        canvas.drawText("起点",startPoint.x,startPoint.x,textPaint);
-        canvas.drawText("终点",endPoint.x,endPoint.x,textPaint);
+//        paint.setColor(Color.GREEN);
+//        paint.setStrokeWidth(3);
+//        paint.setStyle(Paint.Style.FILL);
+//        canvas.drawCircle(startPoint.x,startPoint.y,10,paint);
+//        canvas.drawCircle(endPoint.x,endPoint.y,10,paint);
+        //外圆
+//        paint.setStyle(Paint.Style.STROKE);
+//        canvas.drawCircle(startPoint.x,startPoint.y,10,paint);
+//        canvas.drawCircle(endPoint.x,endPoint.y,10,paint);
 
-        paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(3);
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawCircle(startPoint.x,startPoint.y,6,paint);
-        canvas.drawCircle(endPoint.x,endPoint.y,6,paint);
+        Bitmap startMark = BitmapFactory.decodeResource(getResources(), R.drawable.route_strategy_map_start);
+        canvas.drawBitmap(startMark,startPoint.x-10,startPoint.y-10,paint);
+        Bitmap endMark = BitmapFactory.decodeResource(getResources(), R.drawable.route_strategy_map_end);
+        canvas.drawBitmap(endMark,endPoint.x-12,endPoint.y-40,paint);
 
-
-        paint.setStyle(Paint.Style.STROKE);
-        canvas.drawCircle(startPoint.x,startPoint.y,10,paint);
-        canvas.drawCircle(endPoint.x,endPoint.y,10,paint);
         return bitmap;
+
     }
 
     private Paint updatePaint(){
--- app/src/main/java/com/sen/learn/PrimaryTestActivity.java
+++ app/src/main/java/com/sen/learn/PrimaryTestActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import com.sen.learn.fragment.CardLayoutFragment;
 import com.sen.view.fragment.CanvasFragment;
 import com.sen.view.fragment.CanvasLineFragment;
+import com.sen.view.fragment.CanvasPathFragment;
 import com.sen.view.fragment.LayoutAnimationFragment;
 
 
@@ -28,7 +29,7 @@ public class PrimaryTestActivity extends AppCompatActivity {
         Fragment fragment = manager.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
-            fragment = new CardLayoutFragment();
+            fragment = new CanvasLineFragment();
             manager.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
